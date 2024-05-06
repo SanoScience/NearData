47c593c1633383e0d11208b05c35b59d7bb28c43
piotrkica <piotr.kica.35@gmail.com>
2024-05-06 10:32:42 +0200

    update db_dump.py

--- TranscriptomicsAtlas/analysis/db_dump.py
+++ TranscriptomicsAtlas/analysis/db_dump.py
@@ -19,27 +19,65 @@ def dump_metadata_table(table_name):
             break
 
     df = pd.DataFrame(db)
-    df = df[["SRR_id",
-             "bucket",
-             "tissue_name",
-             "error_type",
-             "salmon_mapping_rate [%]",
-             "SRR_filesize_bytes",
-             "fastq_filesize_bytes",
-             "execution_mode",
-             "instance_id",
-             "prefetch_start_time",
-             "prefetch_end_time",
-             "fasterq_dump_start_time",
-             "fasterq_dump_end_time",
-             "salmon_start_time",
-             "salmon_end_time",
-             "deseq2_start_time",
-             "deseq2_end_time"]]
-    df = df.sort_values(["tissue_name", "salmon_mapping_rate [%]"], ascending=[True, False]).reset_index(drop=True)
-    df.to_csv("data/metadata_db_dump_HPC.csv")
+    print(df.columns)
+    cols_order_star = ["SRR_id",
+                       "tissue_name",
+                       "STAR_mapping_rate [%]",
+                       "SRR_filesize_bytes",
+                       "fastq_filesize_bytes",
+                       "bam_filesize_bytes",
+                       "index_release",
+                       "n_spots",
+                       "nproc",
+                       "instance_type",
+                       "library_layout",
+                       "instance_id",
+                       "execution_mode",
+                       "s3_path",
+                       "prefetch_start_time",
+                       "prefetch_end_time",
+                       "fasterq_dump_start_time",
+                       "fasterq_dump_end_time",
+                       "star_start_time",
+                       "star_end_time",
+                       "deseq2_star_start_time",
+                       "deseq2_star_end_time",
+                       "EBS_Iops",
+                       "EBS_Size",
+                       "EBS_Throughput",
+                       "EBS_VolumeType",
+                       ]
+    cols_order_salmon = ["SRR_id",
+                         "tissue_name",
+                         "salmon_mapping_rate [%]",
+                         "SRR_filesize_bytes",
+                         "fastq_filesize_bytes",
+                         "index_release",
+                         "n_spots",
+                         "nproc",
+                         "instance_type",
+                         "library_layout",
+                         "instance_id",
+                         "execution_mode",
+                         "s3_path",
+                         "prefetch_start_time",
+                         "prefetch_end_time",
+                         "fasterq_dump_start_time",
+                         "fasterq_dump_end_time",
+                         "salmon_start_time",
+                         "salmon_end_time",
+                         "deseq2_salmon_start_time",
+                         "deseq2_salmon_end_time"]
+
+    cols_order = cols_order_star
+    if "error_type" in df.columns:
+        cols_order = cols_order[:4] + ["error_type"] + cols_order[4:]
+
+    df = df[cols_order + (list(set(df.columns).difference(cols_order)))]
+    df = df.sort_values(["tissue_name", "STAR_mapping_rate [%]"], ascending=[True, False]).reset_index(drop=True)
+    df.to_csv(f"data/{table_name}-spot.csv")
 
     return df
 
 
-dump_metadata_table("neardata-tissues-salmon-metadata")
+dump_metadata_table("neardata-test-table-7k")
