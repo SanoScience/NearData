6b4dbade5e9165629b4ad285b78ac1c8a6092a1b
piotrkica <piotr.kica.35@gmail.com>
2025-02-26 13:26:26 +0100

    tf: compact network.tf

--- TranscriptomicsAtlas/Terraform/network.tf
+++ TranscriptomicsAtlas/Terraform/network.tf
@@ -10,38 +10,20 @@ resource "aws_vpc" "NearData_VPC" {
 
 variable "subnets" {
   default = {
-    "Subnet_1" = {
-      "cidr_block"        = "10.0.1.0/24"
-      "availability_zone" = "us-east-1a"
-    }
-    "Subnet_2" = {
-      "cidr_block"        = "10.0.2.0/24"
-      "availability_zone" = "us-east-1b"
-    }
-    "Subnet_3" = {
-      "cidr_block"        = "10.0.3.0/24"
-      "availability_zone" = "us-east-1c"
-    }
-    "Subnet_4" = {
-      "cidr_block"        = "10.0.4.0/24"
-      "availability_zone" = "us-east-1d"
-    }
-    "Subnet_5" = {
-      "cidr_block"        = "10.0.5.0/24"
-      "availability_zone" = "us-east-1e"
-    }
-    "Subnet_6" = {
-      "cidr_block"        = "10.0.6.0/24"
-      "availability_zone" = "us-east-1f"
-    }
+    "Subnet_1" = { cidr = "10.0.1.0/24", az = "us-east-1a" }
+    "Subnet_2" = { cidr = "10.0.2.0/24", az = "us-east-1b" }
+    "Subnet_3" = { cidr = "10.0.3.0/24", az = "us-east-1c" }
+    "Subnet_4" = { cidr = "10.0.4.0/24", az = "us-east-1d" }
+    "Subnet_5" = { cidr = "10.0.5.0/24", az = "us-east-1e" }
+    "Subnet_6" = { cidr = "10.0.6.0/24", az = "us-east-1f" }
   }
 }
 
 resource "aws_subnet" "NearData_Subnets" {
   for_each          = var.subnets
   vpc_id            = aws_vpc.NearData_VPC.id
-  cidr_block        = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
+  cidr_block        = each.value["cidr"]
+  availability_zone = each.value["az"]
 
   tags = {
     Name = "NearData_${each.key}"
